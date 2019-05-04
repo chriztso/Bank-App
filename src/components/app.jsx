8ca71123d56fccfd232125362616abfa2ac794a8
Chris Tso <christophertso2017@u.northwestern.edu>
2019-05-04 15:43:13 -0500

    Set up initial app file

---
+++ src/components/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            login: null
+        }
+    }
+    render(){
+        return (
+            <div>Hello</div>
+        )
+    }
+}
+
+export default App;
